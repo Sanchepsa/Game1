845c664dbfbfbfa7e9bcadb5cbf3962fecffef2b
Sanchepsa <75080060+Sanchepsa@users.noreply.github.com>
2021-02-16 21:29:49 +0300

    Update main.py
    High score table and Input box added.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import os, sys, pygame, time
 from random import randint
+import sqlite3
 
+con = sqlite3.connect('records.db')
+cur = con.cursor()
+result = cur.execute("""SELECT * FROM topten""").fetchall()
+only_once = 0
+input_area = pygame.Rect(100, 100, 140, 32)
+texting_name = ''
+active = False
+color_inactive = pygame.Color('white')
+color_active = pygame.Color((0, 0, 100))
+color = color_inactive
+
+main_input = 'Player 1'
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -45,8 +58,8 @@ rat_right2 = load_image("rat12.png")
 rat_left2 = load_image("rat22.png")
 rats_list = [rat_right, rat_left, rat_right2, rat_left2]
 
-for_play = pygame.font.SysFont('freesansbold.ttf', 40)
-for_play = for_play.render('PLAY', True, (0, 0, 0))
+for_play_font = pygame.font.SysFont('freesansbold.ttf', 40)
+for_play = for_play_font.render('PLAY', True, (0, 0, 0))
 for_loss = pygame.font.SysFont('freesansbold.ttf', 40)
 for_loss = for_loss.render('GAME OVER', True, (0, 0, 0))
 
@@ -196,15 +209,30 @@ rat = Rats(all_sprites)
 my_bull = Bullet(all_sprites)
 
 running = True
+button_play = pygame.draw.rect(screen, (0, 100, 200), (width / 2 - 100, height / 2 - 50, 200, 100),
+                         border_radius=20)
+input_area = pygame.draw.rect(screen, (255, 255, 255), (width / 2 + 50, height / 2 + 90, 200, 50))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running  = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if now_go != 2:
-                if pygame.mouse.get_pos() >= (width / 2 - 100, height / 2 - 50):
-                    if pygame.mouse.get_pos() <= (width / 2 + 100, height / 2 + 50):
-                        now_go = 1
+                if button_play.collidepoint(event.pos):
+                    now_go = 1
+                if input_area.collidepoint(event.pos):
+                    active = not(active)
+                else:
+                    active = False
+                color = color_active if active else color_inactive
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    now_go = 1
+                elif event.key == pygame.K_BACKSPACE:
+                    texting_name = texting_name[:-1]
+                elif len(texting_name) < 8:
+                    texting_name += event.unicode
     screen.blit(back, (0, 0))
     if now_go == 1:
         pygame.draw.rect(screen, (255, 0, 0), (20, 450, 300, 30), border_radius=5)
@@ -212,20 +240,60 @@ while running:
         all_sprites.draw(screen)
         my_cheese.mask = pygame.mask.from_surface(my_cheese.image)
         font_score = pygame.font.SysFont('freesansbold.ttf', 40)
-        txt_score = font_score.render('Score:   ' + str(score), True, (0, 0, 0))
+        txt_score = font_score.render('Score:  ' + str(score), True, (0, 0, 0))
         screen.blit(txt_score, (500, 450))
         if health == 0:
             now_go = 2
         all_sprites.update(event)
     elif now_go == 0:
-        pygame.draw.rect(screen, (0, 100, 200), (width / 2 - 100, height / 2 - 50, 200, 100),
+        button_play = pygame.draw.rect(screen, (0, 100, 200), (width / 2 - 100, height / 2 - 50, 200, 100),
                          border_radius=20)
+        pygame.draw.rect(screen, color, input_area, 2)
+        in_input = for_play_font.render(texting_name, True, color)
+        please_so_on = for_play_font.render('You can enter your name: ', True, 'white')
+        screen.blit(please_so_on, (input_area.x - 380, input_area.y + 12))
+        input_area.w = 280
+        screen.blit(in_input, (input_area.x + 5, input_area.y + 13))
+        pygame.draw.rect(screen, color, input_area, 2)
         screen.blit(for_play, (width / 2 - 35, height / 2 - 15))
     else:
-        pygame.draw.rect(screen, (0, 100, 200), (width / 2 - 140, height / 2 - 100, 300, 200),
+        if texting_name != '':
+            main_input = texting_name
+        pygame.draw.rect(screen, (0, 100, 200), (width / 2 - 300, height / 2 - 100, 300, 200),
                          border_radius=20)
-        screen.blit(for_loss, (width / 2 - 70, height / 2 - 60))
-        screen.blit(txt_score, (width / 2 - 52, height / 2 + 20))
+        screen.blit(for_loss, (width / 2 - 235, height / 2 - 60))
+        screen.blit(txt_score, (width / 2 - 215, height / 2 + 20))
+        pygame.draw.rect(screen, (0, 150, 230), (400, 100, 250, 300))
+        for i in range(100, 351, 50):
+            pygame.draw.rect(screen, (0, 0, 0), (400, i, 250, 50), 2)
+        tab_title = pygame.font.SysFont('freesansbold.ttf', 40)
+        tab_title = tab_title.render('High score table', True, (0, 0, 0))
+        screen.blit(tab_title, (415, 110))
+        str1, str2, str3, str4, str5 = 0, 0, 0, 0, 0
+        scoresList = [str1, str2, str3, str4, str5]
+        h1 = 160
+        if score >= result[4][2] and only_once == 0:
+            for i in range(0, 5):
+                if score >= result[i][2]:
+                    where = i + 1
+                    cur.execute("""UPDATE topten SET name = '{}' WHERE id = {}""".format(main_input, where))
+                    cur.execute("""UPDATE topten SET result = {} WHERE id = {}""".format(score, where))
+                    where += 1
+                    for j in range(where, 6):
+                        cur.execute("""UPDATE topten SET name = '{}' WHERE id = {}""".format(result[j - 2][1], j))
+                        cur.execute("""UPDATE topten SET result = {} WHERE id = {}""".format(result[j - 2][2], j))
+                        con.commit()
+                    break
+            only_once = 1
+        result = cur.execute("""SELECT * FROM topten""").fetchall()
+        for numb, el in enumerate(scoresList):
+            el = pygame.font.SysFont('freesansbold.ttf', 40)
+            txt = str(result[numb][0]) + '. ' + str(result[numb][1]) +\
+                  ': ' + str(result[numb][2])
+            el = el.render(txt, True, (0, 0, 0))
+            screen.blit(el, (415, h1))
+            h1 += 50
     pygame.display.flip()
     clock.tick(60)
+con.close()
 pygame.quit()
